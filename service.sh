20802c8e00d57ea9f527e0fdb9fd0ae008cb2bae
snashiha39 <snashiha@gmail.com>
2025-04-16 13:52:45 +0000

    Add a existing file

--- service.sh
+++ service.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if systemctl is-active --quiet httpd;then
-	echo "httpd is running successfully in port 80"
-ielse
+	echo "httpd is running successfully"
+else
 	systemctl start httpd
 	systemctl enable httpd
+	systemctl status httpd
 fi 
